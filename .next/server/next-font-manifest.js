ef524263f9f19de77463242ac250b2136d91d2fd
fgojkovic <fico.gojkovic@gmail.com>
2026-05-14 09:40:35 +0200

    Removed unnecesarry files. Updated git ignore.

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/filipgojkovic/tezos-bakery-webpage-js/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
